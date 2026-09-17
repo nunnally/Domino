6e3980b345f5f92a3945337e47045aeab1b1a308
César Augusto <sur.cesar@gmail.com>
2026-09-17 00:05:46 -0300

    fix: Mesmo erro
    Ununsed var lint

--- src/features/rankings/HeadToHeadPage.tsx
+++ src/features/rankings/HeadToHeadPage.tsx
@@ -1,7 +1,8 @@
 import { Swords } from 'lucide-react'
 
 import { DominoTile } from '../../components/DominoTile'
-import { getHeadToHeadStats, getPartnerImpactRankings } from '../../lib/stats'
+// import { getHeadToHeadStats, getPartnerImpactRankings } from '../../lib/stats'
+import { getHeadToHeadStats } from '../../lib/stats'
 import type { Game, Player } from '../../lib/types'
 // import { PartnerImpactSection } from './PartnerImpactSection'
 
